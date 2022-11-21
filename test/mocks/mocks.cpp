b0b1fb3abab8dba53ace0ac96bde0101d1e1a5b1
cry1s <cry1s@ya.ru>
2022-11-21 19:56:52 +0300

    uhuuuuuuuuuuuu

--- test/mocks/mocks.cpp
+++ test/mocks/mocks.cpp
@@ -13,15 +13,15 @@ using milliseconds = std::chrono::milliseconds;
 
 class MockIdleManager : public IdleManager {
  public:
-  MOCK_METHOD(milliseconds, takeSleepTime, (), (override));
-  MOCK_METHOD(void, process_idling, (), (override));
+  MOCK_METHOD(milliseconds, takeSleepTime, (), ());
+  MOCK_METHOD(void, process_idling, (), ());
 };
 
 class MockConfig : public Config {
  public:
   MOCK_METHOD(std::string, get_value,
               (const std::string& service, const std::string& parameter),
-              (override));
+              ());
 };
 
 // test for IdleManager
@@ -34,8 +34,8 @@ TEST(IdleManager, takeSleepTime) {
 // test for Config
 TEST(Config, get_value) {
   MockConfig mockConfig;
-  EXPECT_CALL(mockConfig, get_value<std::string>("service", "name")).Times(1);
-  mockConfig.get_value<std::string>("service", "name");
+  EXPECT_CALL(mockConfig, get_value("service", "name")).Times(1);
+  mockConfig.get_value("service", "name");
 }
 
 int main(int argc, char** argv) {
